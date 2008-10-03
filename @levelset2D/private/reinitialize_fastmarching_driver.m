054075447a0922b0449fe6de679475a86eed5879
Gunnar Läthén <gunnar@lathen.se>
2008-10-03 09:30:05 +0000

    Safer reinit procedure to ensure continous distance function

--- @levelset2D/private/reinitialize_fastmarching_driver.m
+++ @levelset2D/private/reinitialize_fastmarching_driver.m
@@ -1,12 +1,14 @@
 
 function ls = reinitialize_fastmarching_driver(ls)
 
-band = ls.band;
+%band = ls.band;
 [phi, ls.band] = reinitialize_fastmarching(ls.phi, ls.band, ls.bandwidth);
 
-A = zeros(size(band));
-A(ls.phi(band) > 0) = ls.bandwidth;
-A(ls.phi(band) < 0) = -ls.bandwidth;
+%A = zeros(size(band));
+%A(ls.phi(band) > 0) = ls.bandwidth;
+%A(ls.phi(band) < 0) = -ls.bandwidth;
+%ls.phi(band) = A;
 
-ls.phi(band) = A;
+ls.phi(ls.phi > 0) = ls.bandwidth;
+ls.phi(ls.phi < 0) = -ls.bandwidth;
 ls.phi(ls.band) = phi;
