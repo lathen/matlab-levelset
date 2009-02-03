d978b3ef47730374e14fdccc7dc94911ffe81d28
Thord Andersson <thord.andersson@liu.se>
2009-02-03 18:35:38 +0000

    Tests with special RPROP

--- @levelset2D/propagate_special_rprop.m
+++ @levelset2D/propagate_special_rprop.m
@@ -1,13 +1,23 @@
-function [ls, iterations, elapsed] = propagate_rprop(ls, time, LR_MAX, LR_MIN, LR_0, top, first_time, operator, varargin)
+function [ls, curv_sum] = propagate_rprop(ls, time, LR_MAX, LR_MIN, LR_0, top, first_time, operator, varargin)
 
 acc_factor = 1.2; %Constant
 dec_factor = 0.5; %Constant
 
-alpha = 1;
-beta  = 0.05;  %Gradient at fi==0 for the "anti-contour field"
-w     = 5; %Inverse-Width of "anti-contour field" (large w --> field close to contour)
-gamma = 4;
-delta = 0;
+% 1, 0.05, 5, 4, 0 works well (L)
+% 1, 0.05, 5, 2, 2 works well, better? (L)
+% 1, 0.4 , 2, 2, 2 works ok (retina)
+alpha   = 1;
+beta    = 0.4;  %Gradient at fi==0 for the "shrinking field"
+w       = 2; %Inverse-Width of "shrinking field" (large w --> field close to contour)
+gamma   = 2;
+delta   = 2;
+epsilon = 0;
+
+avgw = 3;
+medw = 3;
+sigw = 3;
+sigk = 5;
+calc_curv = true;
 
 % Set some persistent variables to store momentum for next call
 persistent old_grad_phi; %Old gradient
@@ -28,7 +38,7 @@ if(first_time)
     [XI,YI] = meshgrid(1:ncols,1:nrows);
     XI = double(XI);
     YI = double(YI);
-    h_av = fspecial('average',[3 3]);
+    h_av = fspecial('average',[avgw avgw]);
     
     %% NB!! We assume static f here! Must change this if steering
     %% information changes!
@@ -37,24 +47,32 @@ if(first_time)
     fneg = f < 0;  %The mask for negative f
 end
 
-% Start counters
-elapsed = 0;
-iterations = 0;
-
 % Save current level set phi and level set band
 old_phi  = ls.phi;
 old_band = ls.band;
 
 mean_im = imfilter(ls.phi,h_av,'symmetric');
+med_im  = colfilt(ls.phi,[medw medw],'sliding',@mykern_med);
+sig_im       = sigf(ls.phi,sigk);
+sig_prime_im = sigf_prime(ls.phi,sigk); 
+sig_mean_im  = colfilt(sig_im,[sigw sigw],'sliding',@mykern_mean);
+
+curv_sum = 0;
+if(calc_curv)
+    cont_im = colfilt(sign(ls.phi),[3 3],'sliding',@mykern);%Calculate curvature.
+    curv_sum = sum(sum(cont_im));
+    figure(46);imagesc(cont_im);colorbar
+end
 
 a_im = zeros(size(ls));
 a_im(fpos) = 2*f(fpos).*(1-ls.phi(fpos));%Calculate gradient for positive f: 2*f*(1-fi)
 a_im(fneg) = 2*f(fneg).*(1+ls.phi(fneg));%Calculate gradient for negative f: 2*f*(1+fi)
 b_im       = 1./(1+(w*ls.phi).^2);
 g_im       = -1*(ls.phi-mean_im);
-d_im       = -1*ls.phi;
+d_im       = -1*(ls.phi-med_im);
+e_im       = -1*(sig_im - sig_mean_im).*sig_prime_im;
 
-curr_grad_phi = alpha*a_im + beta*b_im + gamma*g_im + delta*d_im;
+curr_grad_phi = alpha*a_im + beta*b_im + gamma*g_im + delta*d_im + epsilon*e_im;
 
 %Old anti-contour implementation, v2 :-)
 %cont_im = colfilt(sign(ls.phi),[3 3],'sliding',@mykern);
@@ -101,7 +119,7 @@ subplot(3,3,4);imagesc(g_im);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,5);imagesc(d_im);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,6);imagesc(curr_grad_phi);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,7);imagesc(delta_phi);colorbar;hold on; %plot(ls, 'contour y');
-subplot(3,3,8);imagesc(grad_sprod);colorbar;hold on; %plot(ls, 'contour y');
+subplot(3,3,8);imagesc(e_im);colorbar;hold on; %plot(ls, 'contour y');
 %subplot(4,2,6);imagesc(cont_im);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,9);imagesc(ls.phi);colorbar;hold on; %plot(ls, 'contour y');
 
@@ -113,7 +131,7 @@ subplot(3,3,4);imagesc(gamma*g_im);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,5);imagesc(delta*d_im);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,6);imagesc(curr_grad_phi);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,7);imagesc(delta_phi);colorbar;hold on; %plot(ls, 'contour y');
-subplot(3,3,8);imagesc(grad_sprod);colorbar;hold on; %plot(ls, 'contour y');
+subplot(3,3,8);imagesc(epsilon*e_im);colorbar;hold on; %plot(ls, 'contour y');
 %subplot(4,2,6);imagesc(cont_im);colorbar;hold on; %plot(ls, 'contour y');
 subplot(3,3,9);imagesc(ls.phi);colorbar;hold on; %plot(ls, 'contour y');
 
@@ -122,10 +140,29 @@ if(first_time)
    pause;
 end
 
+%Calculates the number of sign-shifts compared to center-pixel
 function [y] = mykern(x)
 nrows = size(x,1);
 center = ceil(nrows/2.0);
 a = repmat(x(center,:),[nrows 1]);
 b = a.*x;
 y = sum(b < 0);
+
+%Cauclates the median of the neighborhood (without center)
+function [y] = mykern_med(x)
+nrows = size(x,1);
+center = ceil(nrows/2.0);
+y = median(x([1:(center-1) (center+1):end],:));
+
+%Cauclates the mean of the neighborhood (without center)
+function [y] = mykern_mean(x)
+nrows = size(x,1);
+center = ceil(nrows/2.0);
+y = mean(x([1:(center-1) (center+1):end],:));
+
+function [y] = sigf(x,sigk)
+y = atan(sigk*x)/sigk;
+
+function [y] = sigf_prime(x,sigk)
+y = 1./((sigk*x).^2+1);
 
